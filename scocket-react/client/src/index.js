9ee87fc34aa716ad951d0ef3ff2534238bdc0218
mehardil <mehardil13302@gmail.com>
2023-03-09 05:02:34 -0800

    add

---
+++ scocket-react/client/src/index.js
@@ -0,0 +1,28 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import { ContextProvider } from './Context';
+
+import './styles.css';
+
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'),
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+
+// import './styles.css';
+
+// ReactDOM.render(
+// <App/>,
+//   document.getElementById('root'),
+// );
